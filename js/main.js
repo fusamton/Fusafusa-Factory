da7906a4b9443488a5ef9402ae7678e7959883e6
fusamton <168317888+fusamton@users.noreply.github.com>
2024-05-12 15:08:56 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -16,20 +16,40 @@
   let usingWord = '';
   let usingCapitalWord = '';
   let usingSymbolList = '';
-
+  let creatingPassword = '';
 
   // 検証エリア
 
 
   // パスワード生成エリア
-  refresh.addEventListener('click', () => {
-    let creatingPassword = '';
+  function generatPassword() {
+    creatingPassword = '';
     usingWord = wordList + usingCapitalWord + usingSymbolList;
     for (let i = 0; i < passwordLength.value; i++) {
       const randomNumber = Math.floor(Math.random() * usingWord.length);
       creatingPassword += usingWord[randomNumber];
     }
     password.textContent = creatingPassword;
+  }
+
+  function lowerCaseCheck() {
+    let lowerCaseCount = 0;
+      for (let i = 0; i < passwordLength.value; i++) {
+        for (let j = 0; j < 26; j++) {
+          if (creatingPassword[i] === wordList[j]) {
+            lowerCaseCount++;
+          }
+        }
+      }
+    console.log(lowerCaseCount);
+    return lowerCaseCount;
+  }
+
+  refresh.addEventListener('click', () => {
+    generatPassword();
+    while (capitalCheck() === 0 || symbolCheck() === 0 || lowerCaseCheck() === 0) {
+      generatPassword();
+    }
   });
 
   // コピーボタンエリア
@@ -45,6 +65,23 @@
   });
 
   // 大文字使用エリア
+  function capitalCheck() {
+    let capitalCount = 0;
+    if (useCapital.checked) {
+      for (let i = 0; i < passwordLength.value; i++) {
+        for (let j = 0; j < 26; j++) {
+          if (creatingPassword[i] === usingCapitalWord[j]) {
+            capitalCount++;
+          }
+        }
+      }
+    } else {
+      capitalCount++;
+    }
+    console.log(capitalCount);
+    return capitalCount;
+  }
+
   useCapital.addEventListener('change', () => {
     if (useCapital.checked) {
       usingCapitalWord += wordList.toUpperCase();
@@ -54,6 +91,23 @@
   });
 
   // 記号使用エリア
+  function symbolCheck() {
+    let symbolCount = 0;
+    if (useSymbol.checked) {
+      for (let i = 0; i < passwordLength.value; i++) {
+        for (let j = 0; j < usingSymbolList.length; j++) {
+          if (creatingPassword[i] === usingSymbolList[j]) {
+            symbolCount++;
+          }
+        }
+      }
+    } else {
+      symbolCount++;
+    }
+    console.log(symbolCount);
+    return symbolCount;
+  }
+
   function symbolInputToggle() {
     if (useSymbol.checked) {
       usingSymbol.removeAttribute('disabled');
@@ -63,7 +117,7 @@
   }
 
   useSymbol.addEventListener('change', () => {
-    symbolInputToggle()
+    symbolInputToggle();
   });
 
   // 使用記号管理エリア
